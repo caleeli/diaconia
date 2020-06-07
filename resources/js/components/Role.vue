32f6e9b701f020df8e077ecbbd9f95ca56842276
David Callizaya <davidcallizaya@gmail.com>
2020-06-07 11:14:58 -0400

    Agregar configuracion submenues

--- resources/js/components/Role.vue
+++ resources/js/components/Role.vue
@@ -2,7 +2,11 @@
   <div>
     <tabla :fields="fields" :form-fields="formFields" :api="api" :title="__('Role')"
       :search-in="['attributes.name']"
+      :params="{order_by:'parent,id'}"
     >
+      <template v-slot:cell(id)="data">
+        {{ data.item.attributes.parent ? `${data.item.attributes.parent}.${data.item.id}` : data.item.id }}
+      </template>
       <template v-slot:cell(attributes.icon)="data">
         <i :class="data.item.attributes.icon"></i>
       </template>
@@ -21,19 +25,48 @@ export default {
       user: {},
       api: this.$api[`role/${this.role.id}/menus`],
       fields: [
-        {key:'attributes.code', label: 'Code'},
+        {key:'id', label: '#'},
+        {key:'attributes.code', label: 'Código'},
         {key:'attributes.name', label: 'Nombre'},
         {key:'attributes.icon', label: 'Icono' },
         {key:'actions', label: ''},
       ],
       formFields: [
+        {
+          key:'attributes.parent',
+          label: 'Padre',
+          create: true,
+          edit: true,
+          component: "b-select",
+          properties: {
+            options: [],
+          },
+        },
         {key:'attributes.code', label: 'Code', create: true, edit: true },
         {key:'attributes.name', label: 'Nombre', create: true, edit: true },
         {key:'attributes.icon', label: 'Icono', create: true, edit: true, component: 'fa-picker' },
+        {key:'attributes.path', label: 'Ruta', create: true, edit: true },
       ],
     };
   },
-  methods: {
+  mounted() {
+    this.$api[`role/${this.role.id}/menus`]
+      .index({ per_page:-1, where: ['whereNull,parent'] })
+      .then((res) => {
+        const options = [
+          {
+            value: null,
+            text: '',
+          }
+        ];
+        res.data.data.forEach((menu) => {
+          options.push({
+            value: menu.id,
+            text: menu.attributes.name,
+          });
+        });
+        this.formFields[0].properties.options = options;
+      });
   },
 };
 </script>
