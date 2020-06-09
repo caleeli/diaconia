6cdef86972cdc932af24d3ae58d1902ce4599250
David Callizaya <davidcallizaya@gmail.com>
2020-06-08 22:59:52 -0400

    Agregar seeder para operaciones

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesSeeder::class);
         $this->call(UsersSeeder::class);
         $this->call(CombosSeeder::class);
+        $this->call(OperacionesSeeder::class);
     }
 }
