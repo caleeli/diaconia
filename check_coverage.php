076fa7e6e3c953e785b33b6ff28288ae6c5e498e
David Callizaya <davidcallizaya@gmail.com>
2020-06-22 10:44:59 -0400

    Lee la cobertura de codigo

---
+++ check_coverage.php
@@ -0,0 +1,21 @@
+#!/usr/bin/env php
+<?php
+function get_php_coverage() {
+    $coverage = new DomDocument();
+    $coverage->loadHTMLFile('coverage/index.html', LIBXML_NOERROR);
+    $xpath = new DOMXpath($coverage);
+    $percentage = $xpath->query('/html/body/div/div/table/tbody/tr[1]/td[3]/div')->item(0);
+    return $percentage ? $percentage->nodeValue : 'ERROR';
+}
+$coverage = get_php_coverage();
+echo "Cobertura código PHP: {$coverage}\n";
+
+function get_js_coverage() {
+    $coverage = new DomDocument();
+    $coverage->loadHTMLFile('coverage/lcov-report/index.html', LIBXML_NOERROR);
+    $xpath = new DOMXpath($coverage);
+    $percentage = $xpath->query('/html/body/div[1]/div[1]/div/div[1]/span')->item(0);
+    return $percentage ? $percentage->nodeValue : 'error';
+}
+$coverage = get_js_coverage();
+echo "Cobertura código JS: {$coverage}\n";
