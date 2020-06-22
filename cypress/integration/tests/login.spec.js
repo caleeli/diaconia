84b841fd3a8fa1b3693230c1282589967d853fb7
David Callizaya <davidcallizaya@gmail.com>
2020-06-22 08:20:08 -0400

    Mejorar validacion

--- cypress/integration/tests/login.spec.js
+++ cypress/integration/tests/login.spec.js
@@ -4,8 +4,6 @@ context('Login del Sistema', () => {
 
     it('Login de usuario', () => {
         cy.login('admin@coredump.com', 'admin');
-        cy.get('.panel-title').should((panel) => {
-            expect(panel.first()).to.contain('Inicio');
-        });
+        cy.get('.panel-title').should('contain.html', 'Inicio');
     })
 })
