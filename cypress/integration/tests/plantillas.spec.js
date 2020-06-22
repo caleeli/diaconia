2e7e5eb5021b1fd827bf36e29985ca6d22a4497b
Pancho Loza <pancholoza20@gmail.com>
2020-06-22 08:51:41 -0400

    Pruebas de plantillas

---
+++ cypress/integration/tests/plantillas.spec.js
@@ -0,0 +1,67 @@
+
+context('Plantillas de auditoria', () => {
+
+    beforeEach(() => {
+        cy.login('admin@coredump.com', 'admin');
+        cy.get('[data-cy="menu.configuracion"]').click();
+        cy.get('[data-cy="menu.tipos_auditoria"]').click();
+        cy.wait(1000);
+        cy.wait("@api_get.all");
+    })
+
+    it('Lista de plantillas y actualizar cambios', () => {
+        cy.get('[data-cy="tabla.row.edit"]:first').click();
+        cy.get('[data-cy="field.attributes.name"]').clear().type('Nuevo crédito individual');
+        // click en guardar
+        cy.guardar();
+        // esperar a recarga de tabla
+        cy.wait('@api_get');
+        cy.get('[data-cy="tabla.table"]').should('contain.html', 'Nuevo crédito individual');
+
+        // Buscar plantilla especifica
+        cy.buscarEnTabla('Nuevo crédito individual');
+        cy.get('[data-cy="tabla.table"]').find('tbody tr').should('have.length', 1);
+    })
+
+    it('Editar preguntas', () => {
+        cy.get('[data-cy="tabla.row.preguntas"]:first').click();
+        cy.wait('@api_get');
+        cy.get('[data-cy="tabla.row.edit"]:first').click();
+        cy.wait('@api_get');
+        cy.get('[data-cy="field.attributes.descripcion"]').clear().type('Nueva descripción');
+        cy.guardarEnLinea('[data-cy="tabla.row.save"]:first');
+        cy.wait('@api_get');
+        cy.get('[data-cy="tabla.table"]').should('contain.html', 'Nueva descripción');
+    })
+
+    it('Agregar pregunta', () => {
+        cy.get('[data-cy="tabla.row.preguntas"]:first').click();
+        cy.wait('@api_get');
+        cy.wait('@api_get');
+        cy.get('[data-cy="tabla.new"]').click();
+        cy.get('[data-cy="field.attributes.grupo"]').clear().type('5');
+        cy.get('[data-cy="field.attributes.descripcion"]').clear().type('Nueva pregunta');
+        cy.guardarEnLinea('[data-cy="tabla.row.save"]:first', '@api_post');
+        cy.get('[data-cy="tabla.table"]').should('contain.html', 'Nueva pregunta');
+    })
+
+    it('Eliminar pregunta', () => {
+        cy.get('[data-cy="tabla.row.preguntas"]:first').click();
+        cy.wait('@api_get');
+        cy.get('[data-cy="tabla.row.remove"]:first').click();
+        cy.wait('@api_get');
+        cy.si();
+        cy.wait('@api_delete');
+        cy.wait('@api_get');
+        cy.get('[data-cy="tabla.table"]').should('not.contain.html', 'Nueva descripción');
+    })
+
+    /*it('Eliminar plantilla', () => {
+        cy.get('[data-cy="tabla.row.remove"]:first').click();
+        cy.si();
+        cy.wait('@api_delete');
+        // Buscar plantilla especifica
+        cy.buscarEnTabla('Nuevo crédito individual');
+        cy.get('[data-cy="tabla.table"]').find('tbody tr').should('have.length', 0);
+    })*/
+})
