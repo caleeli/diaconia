3ece3079009f0895d1eeff2971b6ed5216120fad
David Callizaya <davidcallizaya@gmail.com>
2020-06-22 11:21:20 -0400

    Fix

--- check_coverage.php
+++ check_coverage.php
@@ -15,7 +15,7 @@ function get_js_coverage() {
     $coverage->loadHTMLFile('coverage/lcov-report/index.html', LIBXML_NOERROR);
     $xpath = new DOMXpath($coverage);
     $percentage = $xpath->query('/html/body/div[1]/div[1]/div/div[1]/span')->item(0);
-    return $percentage ? $percentage->nodeValue : 'error';
+    return $percentage ? trim($percentage->nodeValue) : 'error';
 }
 $coverage = get_js_coverage();
 echo "Cobertura código JS: **{$coverage}**\n";
