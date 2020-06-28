e409f8f1299cb4eeb32cf81bd20f08513d4affa4
Pancho Loza <pancholoza20@gmail.com>
2020-06-27 20:41:22 -0400

    Renombrar migracion

--- database/migrations/2020_05_25_115458_create_riesgos_table.php
+++ database/migrations/2020_05_25_115458_create_riesgos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateComboRiesgoTable extends Migration
+class CreateRiesgosTable extends Migration
 {
     /**
      * Run the migrations.
