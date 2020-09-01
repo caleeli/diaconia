41885260ee903a3322dea1a7d6979c90caf5c0b3
andreaadamczyk <andrea.adamczyk@processmaker.com>
2020-08-31 21:02:34 -0400

    Crear Alerta Cumplimiento

---
+++ app/Alerta.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Alerta extends Model
+{
+    protected $table = 'alertas';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'texto', 'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
